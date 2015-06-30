606bfba21b997da17a2645fd873a342d831429f5
Amanda Carlton <acarlton27@gmail.com>
2015-06-30 10:08:39 -0600

    data

--- routes/index.js
+++ routes/index.js
@@ -13,17 +13,17 @@ var hhCollection = db.get('hh');
 
 router.get('/', function(req, res, next){
   hhCollection.find({},function(err, data){
-    //for(var offset=0; offset=300; offset+20){
-      /*yelp.search({term: "happy hour", location: "denver", offset:440}, function(error, data) {
+    //for(var offset=20; offset=400; offset+20){
+      yelp.search({term: "happy hour", location: "denver"}, function(error, data) {
         console.log(error);
         console.log(data.businesses);
 
-      hhCollection.insert(data.businesses);
-    });
-  //}
+        hhCollection.insert(data.businesses);
+      });
+    //}
     //console.log(data)*/
     res.render('index', {allbars: data, title: "All the Bars"});
- });
+  });
 });
 
 router.get('/bars/:id', function(req,res,next){
@@ -37,32 +37,32 @@ router.post("/new", function(req, res, next){
   console.log(req.body.neighborhood.length);
   console.log(rating);
   if(req.body.rating.length>0 && req.body.neighborhood.length>0){
-  hhCollection.find({rating:rating, "location.neighborhoods":req.body.neighborhood}, function(err, bar){
-  res.render("new",{bar:bar});
+    hhCollection.find({rating:rating, "location.neighborhoods":req.body.neighborhood}, function(err, bar){
+      res.render("new",{bar:bar});
 
- });
+    });
 
- }else if (rating===0 && req.body.neighborhood.length>0){
-  hhCollection.find({"location.neighborhoods":req.body.neighborhood}, function(err, bar){
+  }else if (rating===0 && req.body.neighborhood.length>0){
+    hhCollection.find({"location.neighborhoods":req.body.neighborhood}, function(err, bar){
 
 
-  res.render("new",{bar:bar});
+      res.render("new",{bar:bar});
+    });
+
+  }else{//(rating>0 && req.body.neighborhood.length<0){
+    hhCollection.find({rating:rating}, function(err, bar){
+      res.render("new", {bar:bar});
+    });
+  }
+
 });
 
-}else{//(rating>0 && req.body.neighborhood.length<0){
-  hhCollection.find({rating:rating}, function(err, bar){
- res.render("new", {bar:bar});
+router.post('/bars/:id', function(req, res, next) {
+  hhCollection.update({_id: req.params.id},{ hhurl: req.body.hhurl, hhmenu: req.body.hhmenu , start: req.body.stime, end: req.body.etime},function(err,bar){
+    res.redirect('/bars/'+ req.params.id);
+
   });
-}
-
 });
-
-// router.post('/bars/:id', function(req, res, next) {
-//   hhCollection.update({_id: req.params.id},{ hhurl: req.body.hhurl, hhmenu: req.body.hhmenu , start: req.body.stime, end: req.body.etime},function(err,bar){
-//   res.redirect('/bars/'+ req.params.id);
-//
-// });
-// });
 
 
 
